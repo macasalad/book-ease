6a9d0ffb356fbf87e1206121b983f56459577c73
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2026-02-26 00:35:16 +0800

    Added link to Profile viewer on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation'
 import { SignOutButton } from '../components/SignOutButton'
 import { auth } from '../../../auth'
 import { headers } from 'next/headers'
+import Link from 'next/link'
  
 export default async function Dashboard() {
   const session = await auth.api.getSession({
@@ -13,10 +14,21 @@ export default async function Dashboard() {
   }
  
   return (
+    <main>
+    <nav>
+         <div>
+            <Link href="/profile">
+                Profile
+            </Link>
+         </div>
+         <div>
+            <SignOutButton />
+         </div>
+    </nav>
     <section className="p-10">
-      <h1 className="text-2xl font-bold">Welcome, {session.user.name}!</h1>
-      <p className="mt-2">You made it to the protected area. 🎉</p>
-      <SignOutButton />
+        <h1 className="text-2xl font-bold">Welcome, {session.user.name}!</h1>
+
     </section>
+    </main>
   )
 }
